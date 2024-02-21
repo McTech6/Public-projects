9da1fd2388bdd83855274efa36df1d22e27907dc
Mctech6 <tiddingramsey@gmail.com>
2024-02-21 18:42:06 +0100

    Done with the multipurpose website

--- MultipurposeSite/main.js
+++ MultipurposeSite/main.js
@@ -1,5 +1,5 @@
 //change nav style on scroll
-
+/*
 window.addEventListener('scroll', ()=>{
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY>0);
 })
@@ -12,4 +12,23 @@ faqs.forEach(faq=>{
     faq.addEventListener('click', ()=>{
        faq.classList.toggle('open'); 
     })
+})*/
+
+const menu=document.querySelector('.nav-menu');
+const openBtn= document.querySelector('.open-menu-btn')
+const closeBtn= document.querySelector('.close-menu-btn');
+
+
+
+openBtn.addEventListener('click', ()=>{
+    menu.style.setProperty('display', 'flex');
+    closeBtn.style.setProperty('display', 'inline-block');
+    openBtn.style.setProperty('display', 'none');
+     
+})
+
+closeBtn.addEventListener('click',()=>{
+    menu.style.setProperty('display', 'none');
+    openBtn.style.setProperty('display', 'inline');
+    closeBtn.style.setProperty('display', 'none');
 })
