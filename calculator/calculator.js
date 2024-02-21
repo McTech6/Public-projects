1b4a547958a9ce12a3f01dd4deec4b880c800841
Mctech6 <tiddingramsey@gmail.com>
2024-02-21 12:03:11 +0100

    I upgraded the calculator but not done

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,14 +1,52 @@
-let string= '';
-let buttons= document.querySelectorAll('.btn');
-Array.from(buttons).forEach((button)=>{
-     button.addEventListener('click', (e)=>{
-        if (e.target.innerHTML==='='){
-           string= eval(string);
-           document.querySelector('.input').value=string;
-        } else {
-          console.log(e.target);
-          string += e.target.innerHTML;
-          document.querySelector('.input').value=string;
-        }
-     })
+class calculator{
+   constructor(previousButton,presentButton){
+      this.previousButton= previousButton
+      this.presentButton= presentButton
+      this.clear()
+   }
+
+   clear(){
+        this.presentButton= ''
+        this.previousButton= ''
+        this.operation=undefined
+
+   }
+
+   delete(){
+
+   }
+
+   append(number){
+         this.presentButton =number
+   }
+
+   chooseOperation(operation){
+
+   }
+
+   compute(){
+
+   }
+
+   updateDisplay(){
+       this.presentButton.innerText= this.presentButton     
+   }
+
+}
+
+const numberButtons= document.querySelectorAll('[data-number]')
+const operationButton= document.querySelectorAll('[data-operation]')
+const equalButton= document.querySelector('[data-equal]')
+const deleteButton= document.querySelector('[data-delete]')
+const acButton= document.querySelector('[data-ac]')
+const previousButton= document.querySelector('[data-previous]')
+const presentButton= document.querySelector('[data-present]')
+
+const calculator =new calculator(previousButton, presentButton)
+
+numberButtons.forEach(button => {
+   button.addEventListener('click', ()=>{
+      calculator.append(button.innerText)
+      calculator.updateDisplay()
+   })
 })
